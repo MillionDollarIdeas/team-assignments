b64e754af03266c0cd25b3efe5e4821b7674526e
Jef Spaleta <jspaleta@gmail.com>
2025-01-15 15:47:11 -0900

    add output file to overrides aggregation script

--- tools/aggregate_team_overrides.sh
+++ tools/aggregate_team_overrides.sh
@@ -6,10 +6,30 @@ set -eu
 set -o pipefail
 
 # $1 - directory
+# $2 - output file
+if [ -z "$1" ]; then
+  echo "Error: Missing first argument - teams directory"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "Error: Missing second argument - output file"
+  exit 1
+fi
+
+cdir=`pwd`
+outputfile=$cdir/$2
+
+if [ -f "$outputfile" ]; then
+  echo "Error: File '$outputfile' already exists." 
+  exit 1 
+fi
+
 echo "# Aggregating Teams from $1/*.yaml"
 cd $1
+touch $outputfile
 for file in *.yaml; do
      f="${file%.*}"
-     echo "$f:"
-     sed 's/^/  /' $file
+     echo "$f:" >> $outputfile
+     sed 's/^/  /' $file >> $outputfile
 done

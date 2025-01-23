823bf02abad03090924e28294a51a309d9977236
Jef Spaleta <jspaleta@gmail.com>
2025-01-23 07:34:52 -0900

    add team overrides aggregate script tooling

--- tools/aggregate_team_overrides.sh
+++ tools/aggregate_team_overrides.sh
@@ -2,38 +2,39 @@
 # Copyright Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
+##
+# This script is used to generate an aggregate team membership
+# file based on a directory of yaml files of the 
+# form team-name.yaml
+# This tool is used as part of team-management automation
+##
+
 set -eu
 set -o pipefail
 
-# $1 - directory
-# $2 - output file
-if [ -z "$1" ]; then
-  echo "Error: Missing first argument - teams directory"
+# $1 - teams directory
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <TEAMS_DIRECTORY>"
   exit 1
 fi
 
-if [ -z "$2" ]; then
-  echo "Error: Missing second argument - output file"
-  exit 1
-fi
-
-cdir=`pwd`
-outputfile=$cdir/$2
-
-if [ -f "$outputfile" ]; then
-  echo "Error: File '$outputfile' already exists." 
-  exit 1 
-fi
-
-echo "# Aggregating Teams from $1/*.yaml"
-cd $1
-touch $outputfile
-echo "teams:" >> $outputfile
+>&2 echo "> Aggregating Teams from $1/*.yaml"
+CDIR=`pwd`
+cd "$1"
+echo "teams:" 
 for file in *.yaml; do
+     >&2 echo ">> Processing: $file"
      f="${file%.*}"
-     echo "  $f:" >> $outputfile
-     sed 's/^/    /' $file >> $outputfile
+     cd "$CDIR"; cd "$2"
+     for user in *.yaml; do
+        >&2 echo ">> Checking Special User File: $user"
+        yq ".teams.$f" $user
+     done
+     cd "$CDIR"; cd "$1"
+     >&2 echo "> Aggregating Teams from $1/*.yaml"
+     echo "  $f:" 
+     sed 's/^/    /' "$file"
+     #Add one blank line after each team just to make sure it exists"
+     echo "" 
 done
 
-echo "Final Aggregate file $outputfile"
-cat $outputfile

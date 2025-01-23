708ab93428b82b057562ad1bfd2ce040966fefe5
Jef Spaleta <jspaleta@gmail.com>
2025-01-23 08:56:15 -0900

    add aggregation of special user files

--- tools/aggregate_team_overrides.sh
+++ tools/aggregate_team_overrides.sh
@@ -21,20 +21,56 @@ fi
 >&2 echo "> Aggregating Teams from $1/*.yaml"
 CDIR=`pwd`
 cd "$1"
-echo "teams:" 
+#yq -V
+#yq --help
+#yq m *.yaml
+# Merge all the team files
+#yq eval-all '. as $item ireduce ({}; . * $item )' ladder/teams/*.yaml
+
+aggregate_yaml=$(echo -e "teams:")
 for file in *.yaml; do
      >&2 echo ">> Processing: $file"
      f="${file%.*}"
      cd "$CDIR"; cd "$2"
-     for user in *.yaml; do
-        >&2 echo ">> Checking Special User File: $user"
-        yq ".teams.$f" $user
-     done
      cd "$CDIR"; cd "$1"
      >&2 echo "> Aggregating Teams from $1/*.yaml"
-     echo "  $f:" 
-     sed 's/^/    /' "$file"
+     aggregate_yaml+=$'\n'$(echo -e "  $f:") 
+     aggregate_yaml+=$'\n'$(sed 's/^/    /' "$file")
      #Add one blank line after each team just to make sure it exists"
-     echo "" 
+     aggregate_yaml+=$'\n' 
 done
+temp_agg_file=$(mktemp)
+echo "$aggregate_yaml" > $temp_agg_file 
+
+temp_user_merge_file=$(mktemp)
+temp_user_file=$(mktemp)
+temp_merge_file=$(mktemp)
+cd "$CDIR"; cd "$2"
+for userfile in *.yaml; do
+        echo  'teams:' > $temp_user_file
+        >&2 echo ">> Checking Special User File: $userfile"
+	user="${userfile%.*}"
+        teams=`yq '.teams | keys | .[] ' $userfile`
+	for team in $teams; do
+                echo  "  $team:" >> $temp_user_file
+	        result=`yq ".teams.$team.member" $userfile`
+	        if [ "$result" = "true" ]; then
+                  >&2 echo "user $user is a member of $team"
+		  echo  "    members:" >> $temp_user_file
+		  echo  "      - $user" >> $temp_user_file
+	        fi  
+                result=`yq ".teams.$team.mentor" $userfile`
+	        if [ "$result" = "true" ]; then
+                  >&2 echo "user $user is a mentor of $team"
+		  echo  "    mentors:" >> $temp_user_file
+		  echo  "      - $user" >> $temp_user_file
+	        fi  
+	done
+	yq eval-all '. as $item ireduce ({}; . *+ $item )' $temp_user_file $temp_merge_file > $temp_user_merge_file
+	cat $temp_user_merge_file > $temp_merge_file
+	echo "" > $temp_user_merge_file
+	#cat $temp_merge_file
+done
+
+yq eval-all '. as $item ireduce ({}; . *+ $item )' $temp_agg_file $temp_merge_file 
 
